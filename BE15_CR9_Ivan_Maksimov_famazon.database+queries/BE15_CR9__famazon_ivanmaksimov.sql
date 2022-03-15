be6451a03818efdfb351897a9d76bf70158e7566
ivmaksimov <100725158+ivmaksimov@users.noreply.github.com>
2022-03-15 22:37:02 +0100

    Add files via upload

--- BE15_CR9_Ivan_Maksimov_famazon.database+queries/BE15_CR9__famazon_ivanmaksimov.sql
+++ BE15_CR9_Ivan_Maksimov_famazon.database+queries/BE15_CR9__famazon_ivanmaksimov.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 12, 2022 at 04:11 PM
+-- Generation Time: Mar 15, 2022 at 09:48 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -31,7 +31,6 @@ USE `be15_cr9_famazon_ivanmaksimov`;
 
 CREATE TABLE `acounts` (
   `acount_id` int(11) NOT NULL,
-  `fk_customer_id` int(11) DEFAULT NULL,
   `birth_date` date DEFAULT NULL,
   `username` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL,
@@ -44,19 +43,19 @@ CREATE TABLE `acounts` (
 -- Dumping data for table `acounts`
 --
 
-INSERT INTO `acounts` (`acount_id`, `fk_customer_id`, `birth_date`, `username`, `password`, `mobile_phone`, `last_login`, `fk_adress_id`) VALUES
-(1, 4, '1998-12-06', 'chichiri@googl.win', '*************', 436858, '2021-02-25', 12),
-(2, 8, '1956-02-02', 'griebelerarte@tchoeo.com', '*************', 432547, '2022-05-14', 11),
-(3, 12, '1994-05-26', 'heathermcleish@tchoeo.com', '*************', 436855, '2022-03-11', 10),
-(4, 2, '1949-02-19', 'griebelerarte@tchoeo.com', '*********', 435547, '2022-01-18', 9),
-(5, 6, '1997-12-04', 'edgard80@lyricshnagu.com', '*************', 438558, '2021-01-18', 8),
-(6, 10, '1964-05-23', 'taninc22@hungtaoteile.com', '*********', 45687, '2021-04-05', 7),
-(7, 1, '1978-06-28', 'shandacombs@yaungshop.com', '*************', 434458, '2021-06-18', 6),
-(8, 5, '1985-04-30', 'shortspan@nkgursr.com', '*********', 432237, '2021-04-21', 5),
-(9, 9, '1936-04-24', 'mlrampold@lohpcn.com', '*********', 4362218, '2021-08-18', 4),
-(10, 3, '1987-08-01', 'timpx1@eluvit.com', '*********', 432587, '2021-09-21', 3),
-(11, 7, '1974-12-14', 'donaldsondanielson7@proxy4gs.c', '*************', 437748, '2021-06-23', 2),
-(12, 11, '1968-09-04', 'barkov7777@ellahamid.art', '*********', 437747, '2021-09-30', 1);
+INSERT INTO `acounts` (`acount_id`, `birth_date`, `username`, `password`, `mobile_phone`, `last_login`, `fk_adress_id`) VALUES
+(1, '1998-12-06', 'chichiri@googl.win', '*************', 436858, '2021-02-25', 12),
+(2, '1956-02-02', 'griebelerarte@tchoeo.com', '*************', 432547, '2022-05-14', 11),
+(3, '1994-05-26', 'heathermcleish@tchoeo.com', '*************', 436855, '2022-03-11', 10),
+(4, '1949-02-19', 'griebelerarte@tchoeo.com', '*********', 435547, '2022-01-18', 9),
+(5, '1997-12-04', 'edgard80@lyricshnagu.com', '*************', 438558, '2021-01-18', 8),
+(6, '1964-05-23', 'taninc22@hungtaoteile.com', '*********', 45687, '2021-04-05', 7),
+(7, '1978-06-28', 'shandacombs@yaungshop.com', '*************', 434458, '2021-06-18', 6),
+(8, '1985-04-30', 'shortspan@nkgursr.com', '*********', 432237, '2021-04-21', 5),
+(9, '1936-04-24', 'mlrampold@lohpcn.com', '*********', 4362218, '2021-08-18', 4),
+(10, '1987-08-01', 'timpx1@eluvit.com', '*********', 432587, '2021-09-21', 3),
+(11, '1974-12-14', 'donaldsondanielson7@proxy4gs.c', '*************', 437748, '2021-06-23', 2),
+(12, '1968-09-04', 'barkov7777@ellahamid.art', '*********', 437747, '2021-09-30', 1);
 
 -- --------------------------------------------------------
 
@@ -66,23 +65,22 @@ INSERT INTO `acounts` (`acount_id`, `fk_customer_id`, `birth_date`, `username`, 
 
 CREATE TABLE `companies` (
   `comp_id` int(11) NOT NULL,
-  `name` varchar(30) NOT NULL,
-  `fk_adress_id` int(11) DEFAULT NULL
+  `name` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `companies`
 --
 
-INSERT INTO `companies` (`comp_id`, `name`, `fk_adress_id`) VALUES
-(1, 'Marquardt GmbH', 1),
-(2, 'Heinemann Bernhardt ', 2),
-(3, 'Stoll Stiftung & Co. KGaA', 3),
-(4, 'Conrad Baum GmbH & Co. KG', 4),
-(5, 'Rieger Rauch Stiftung & Co. KG', 5),
-(6, 'Brunner Paul AG', 6),
-(7, 'Bittner GmbH ', 7),
-(8, 'Busse Hauser AG', 8);
+INSERT INTO `companies` (`comp_id`, `name`) VALUES
+(1, 'Marquardt GmbH'),
+(2, 'Heinemann Bernhardt '),
+(3, 'Stoll Stiftung & Co. KGaA'),
+(4, 'Conrad Baum GmbH & Co. KG'),
+(5, 'Rieger Rauch Stiftung & Co. KG'),
+(6, 'Brunner Paul AG'),
+(7, 'Bittner GmbH '),
+(8, 'Busse Hauser AG');
 
 -- --------------------------------------------------------
 
@@ -122,27 +120,55 @@ INSERT INTO `comp_adresses` (`adress_id`, `adress`, `ZIP`, `city`, `country`, `f
 CREATE TABLE `customers` (
   `customer_id` int(11) NOT NULL,
   `first_name` varchar(30) NOT NULL,
-  `last_name` varchar(30) NOT NULL,
-  `fk_acount_id` int(11) NOT NULL
+  `last_name` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `customers`
 --
 
-INSERT INTO `customers` (`customer_id`, `first_name`, `last_name`, `fk_acount_id`) VALUES
-(1, 'Kristian ', 'Dreher', 1),
-(2, 'Diana ', 'Meister', 2),
-(3, 'Jörg ', 'Osterhagen', 3),
-(4, 'Mario', ' Walter', 4),
-(5, 'Michael ', 'Schmitz', 5),
-(6, 'Tim ', 'Wannemaker', 6),
-(7, 'Leonie', ' Bader', 10),
-(8, 'Jessica', ' Sommer', 7),
-(9, 'Marko ', 'Neudorf', 8),
-(10, 'Antje', ' Krause', 9),
-(11, 'Christina ', 'Bauer', 11),
-(12, 'Silke ', 'Vogler', 12);
+INSERT INTO `customers` (`customer_id`, `first_name`, `last_name`) VALUES
+(1, 'Kristian ', 'Dreher'),
+(2, 'Diana ', 'Meister'),
+(3, 'Jörg ', 'Osterhagen'),
+(4, 'Mario', ' Walter'),
+(5, 'Michael ', 'Schmitz'),
+(6, 'Tim ', 'Wannemaker'),
+(7, 'Leonie', ' Bader'),
+(8, 'Jessica', ' Sommer'),
+(9, 'Marko ', 'Neudorf'),
+(10, 'Antje', ' Krause'),
+(11, 'Christina ', 'Bauer'),
+(12, 'Silke ', 'Vogler');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_acounts`
+--
+
+CREATE TABLE `customer_acounts` (
+  `fk_customer_id` int(11) NOT NULL,
+  `fk_acount_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `customer_acounts`
+--
+
+INSERT INTO `customer_acounts` (`fk_customer_id`, `fk_acount_id`) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 6),
+(7, 7),
+(8, 8),
+(9, 9),
+(10, 10),
+(11, 11),
+(12, 12);
 
 -- --------------------------------------------------------
 
@@ -155,27 +181,26 @@ CREATE TABLE `customer_adresses` (
   `adress` varchar(30) NOT NULL,
   `ZIP` int(11) NOT NULL,
   `city` varchar(30) NOT NULL,
-  `country` varchar(30) NOT NULL,
-  `fk_customer_id` int(11) NOT NULL
+  `country` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `customer_adresses`
 --
 
-INSERT INTO `customer_adresses` (`adress_id`, `adress`, `ZIP`, `city`, `country`, `fk_customer_id`) VALUES
-(1, 'Herzoggasse 21', 2340, 'Amstetten', ' Austria', 0),
-(2, 'Erzherzog-Johann-Straße 12', 8967, 'Ansfelden', 'Austria', 0),
-(3, 'Zenogasse 34', 1120, 'Bad Ischl', 'Austria', 0),
-(4, 'Wieden 30', 2070, 'Bad Ischl', 'Austria', 0),
-(5, 'Helfertgasse 35', 1120, 'Braunau am Inn', 'Austria', 0),
-(6, ' Birkenweg 29', 5580, 'Baden bei Wien', 'Austria', 0),
-(7, ' Archenweg 27', 6114, 'Enns', 'Austria', 0),
-(8, ' Mühlenweg 34', 6067, 'Bischofshofen', 'Austria', 0),
-(9, 'Kufsteiner Straße 14', 6336, 'Innsbruck', 'Austria', 0),
-(10, 'Elidagasse 35', 1220, 'Hollabrunn', 'Austria', 0),
-(11, ' Mörikeweg 27', 1160, 'Graz', 'Austria', 0),
-(12, ' Kindergartengasse 10', 2722, 'LInz', 'Austria', 0);
+INSERT INTO `customer_adresses` (`adress_id`, `adress`, `ZIP`, `city`, `country`) VALUES
+(1, 'Herzoggasse 21', 2340, 'Amstetten', ' Austria'),
+(2, 'Erzherzog-Johann-Straße 12', 8967, 'Ansfelden', 'Austria'),
+(3, 'Zenogasse 34', 1120, 'Bad Ischl', 'Austria'),
+(4, 'Wieden 30', 2070, 'Bad Ischl', 'Austria'),
+(5, 'Helfertgasse 35', 1120, 'Braunau am Inn', 'Austria'),
+(6, ' Birkenweg 29', 5580, 'Baden bei Wien', 'Austria'),
+(7, ' Archenweg 27', 6114, 'Enns', 'Austria'),
+(8, ' Mühlenweg 34', 6067, 'Bischofshofen', 'Austria'),
+(9, 'Kufsteiner Straße 14', 6336, 'Innsbruck', 'Austria'),
+(10, 'Elidagasse 35', 1220, 'Hollabrunn', 'Austria'),
+(11, ' Mörikeweg 27', 1160, 'Graz', 'Austria'),
+(12, ' Kindergartengasse 10', 2722, 'LInz', 'Austria');
 
 -- --------------------------------------------------------
 
@@ -269,35 +294,35 @@ INSERT INTO `products` (`product_id`, `name`, `price`, `quantity`) VALUES
 --
 
 CREATE TABLE `product_company` (
-  `fk_product_id` int(11) NOT NULL,
-  `fk_comp_id` int(11) NOT NULL
+  `fk_comp_id` int(11) NOT NULL,
+  `fk_product_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `product_company`
 --
 
-INSERT INTO `product_company` (`fk_product_id`, `fk_comp_id`) VALUES
-(1, 3),
-(2, 4),
-(3, 6),
-(4, 5),
-(5, 7),
-(6, 2),
-(7, 1),
-(8, 1),
-(9, 2),
-(10, 3),
-(11, 8),
-(12, 8),
-(13, 2),
-(14, 3),
-(15, 4),
-(16, 5),
-(17, 6),
-(18, 7),
-(19, 8),
-(20, 1);
+INSERT INTO `product_company` (`fk_comp_id`, `fk_product_id`) VALUES
+(3, 1),
+(4, 2),
+(6, 3),
+(5, 4),
+(7, 5),
+(2, 6),
+(1, 7),
+(1, 8),
+(2, 9),
+(3, 10),
+(8, 11),
+(8, 12),
+(2, 13),
+(3, 14),
+(4, 15),
+(5, 16),
+(6, 17),
+(7, 18),
+(8, 19),
+(1, 20);
 
 -- --------------------------------------------------------
 
@@ -307,40 +332,79 @@ INSERT INTO `product_company` (`fk_product_id`, `fk_comp_id`) VALUES
 
 CREATE TABLE `purchases` (
   `purchase_id` int(11) NOT NULL,
-  `fk_acount_id` int(11) NOT NULL,
   `fk_product_id` int(11) NOT NULL,
   `quantity` varchar(30) NOT NULL,
   `fk_payment_id` int(11) NOT NULL,
-  `purchase_date` date NOT NULL
+  `purchase_date` date NOT NULL,
+  `fk_web_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `purchases`
 --
 
-INSERT INTO `purchases` (`purchase_id`, `fk_acount_id`, `fk_product_id`, `quantity`, `fk_payment_id`, `purchase_date`) VALUES
-(1, 1, 1, '3', 3, '2021-12-23'),
-(2, 2, 20, '1', 1, '2022-01-02'),
-(3, 3, 19, '2', 3, '2021-12-23'),
-(4, 4, 18, '2', 2, '2022-02-02'),
-(5, 5, 17, '1', 1, '2021-09-12'),
-(6, 6, 15, '2', 2, '2020-08-12'),
-(7, 7, 16, '4', 3, '2021-04-06'),
-(8, 8, 14, '1', 1, '2020-08-12'),
-(9, 9, 13, '2', 4, '2020-02-06'),
-(10, 10, 12, '3', 2, '2022-02-17'),
-(11, 11, 12, '1', 3, '2019-12-02'),
-(12, 12, 11, '2', 1, '2019-05-06'),
-(13, 1, 10, '1', 1, '2021-04-06'),
-(14, 2, 9, '1', 3, '2022-02-17'),
-(15, 3, 8, '3', 4, '2021-09-12'),
-(16, 4, 7, '1', 4, '2020-08-12'),
-(17, 5, 7, '3', 3, '2019-07-05'),
-(18, 6, 6, '1', 1, '2022-11-05'),
-(19, 7, 5, '1', 3, '2021-12-23'),
-(20, 9, 4, '1', 1, '2020-08-12'),
-(21, 10, 3, '1', 4, '2021-12-23'),
-(22, 11, 2, '1', 4, '2022-02-02');
+INSERT INTO `purchases` (`purchase_id`, `fk_product_id`, `quantity`, `fk_payment_id`, `purchase_date`, `fk_web_id`) VALUES
+(1, 1, '3', 3, '2021-12-23', 1),
+(2, 20, '1', 1, '2022-01-02', 2),
+(3, 19, '2', 3, '2021-12-23', 3),
+(4, 18, '2', 2, '2022-02-02', 4),
+(5, 17, '1', 1, '2021-09-12', 5),
+(6, 15, '2', 2, '2020-08-12', 6),
+(7, 16, '4', 3, '2021-04-06', 7),
+(8, 14, '1', 1, '2020-08-12', 8),
+(9, 13, '2', 4, '2020-02-06', 9),
+(10, 12, '3', 2, '2022-02-17', 10),
+(11, 12, '1', 3, '2019-12-02', 11),
+(12, 11, '2', 1, '2019-05-06', 12),
+(13, 10, '1', 1, '2021-04-06', 13),
+(14, 9, '1', 3, '2022-02-17', 14),
+(15, 8, '3', 4, '2021-09-12', 15),
+(16, 7, '1', 4, '2020-08-12', 16),
+(17, 7, '3', 3, '2019-07-05', 17),
+(18, 6, '1', 1, '2022-11-05', 18),
+(19, 5, '1', 3, '2021-12-23', 19),
+(20, 4, '1', 1, '2020-08-12', 20),
+(21, 3, '1', 4, '2021-12-23', 21),
+(22, 2, '1', 4, '2022-02-02', 22);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase_acount`
+--
+
+CREATE TABLE `purchase_acount` (
+  `fk_purchase_id` int(11) NOT NULL,
+  `fk_acount_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `purchase_acount`
+--
+
+INSERT INTO `purchase_acount` (`fk_purchase_id`, `fk_acount_id`) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 6),
+(7, 7),
+(8, 8),
+(9, 9),
+(10, 10),
+(11, 11),
+(12, 12),
+(13, 3),
+(14, 6),
+(15, 9),
+(16, 12),
+(17, 2),
+(18, 4),
+(19, 5),
+(20, 7),
+(21, 10),
+(22, 11);
 
 -- --------------------------------------------------------
 
@@ -392,8 +456,9 @@ INSERT INTO `shipments` (`shipping_id`, `fk_comp_id`, `fk_purchase_id`, `shipp_d
 --
 
 CREATE TABLE `website` (
-  `fk_product_id` int(11) NOT NULL,
+  `website_id` int(11) NOT NULL,
   `name` varchar(30) NOT NULL,
+  `fk_product_id` int(11) NOT NULL,
   `web_adress` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -401,27 +466,27 @@ CREATE TABLE `website` (
 -- Dumping data for table `website`
 --
 
-INSERT INTO `website` (`fk_product_id`, `name`, `web_adress`) VALUES
-(1, 'Famazon', 'www.famazon.com'),
-(2, 'Famazon', 'www.famazon.com'),
-(3, 'Famazon', 'www.famazon.com'),
-(4, 'Famazon', 'www.famazon.com'),
-(5, 'Famazon', 'www.famazon.com'),
-(6, 'Famazon', 'www.famazon.com'),
-(7, 'Famazon', 'www.famazon.com'),
-(8, 'Famazon', 'www.famazon.com'),
-(9, 'Famazon', 'www.famazon.com'),
-(10, 'Famazon', 'www.famazon.com'),
-(11, 'Famazon', 'www.famazon.com'),
-(12, 'Famazon', 'www.famazon.com'),
-(13, 'Famazon', 'www.famazon.com'),
-(14, 'Famazon', 'www.famazon.com'),
-(15, 'Famazon', 'www.famazon.com'),
-(16, 'Famazon', 'www.famazon.com'),
-(17, 'Famazon', 'www.famazon.com'),
-(18, 'Famazon', 'www.famazon.com'),
-(19, 'Famazon', 'www.famazon.com'),
-(20, 'Famazon', 'www.famazon.com');
+INSERT INTO `website` (`website_id`, `name`, `fk_product_id`, `web_adress`) VALUES
+(1, 'Famazon', 1, 'www.famazon.com'),
+(2, 'Famazon', 2, 'www.famazon.com'),
+(3, 'Famazon', 3, 'www.famazon.com'),
+(4, 'Famazon', 4, 'www.famazon.com'),
+(5, 'Famazon', 5, 'www.famazon.com'),
+(6, 'Famazon', 6, 'www.famazon.com'),
+(7, 'Famazon', 7, 'www.famazon.com'),
+(8, 'Famazon', 8, 'www.famazon.com'),
+(9, 'Famazon', 9, 'www.famazon.com'),
+(10, 'Famazon', 10, 'www.famazon.com'),
+(11, 'Famazon', 11, 'www.famazon.com'),
+(12, 'Famazon', 12, 'www.famazon.com'),
+(13, 'Famazon', 13, 'www.famazon.com'),
+(14, 'Famazon', 14, 'www.famazon.com'),
+(15, 'Famazon', 15, 'www.famazon.com'),
+(16, 'Famazon', 16, 'www.famazon.com'),
+(17, 'Famazon', 17, 'www.famazon.com'),
+(18, 'Famazon', 18, 'www.famazon.com'),
+(19, 'Famazon', 19, 'www.famazon.com'),
+(20, 'Famazon', 20, 'www.famazon.com');
 
 --
 -- Indexes for dumped tables
@@ -432,15 +497,13 @@ INSERT INTO `website` (`fk_product_id`, `name`, `web_adress`) VALUES
 --
 ALTER TABLE `acounts`
   ADD PRIMARY KEY (`acount_id`),
-  ADD KEY `fk_adress_id` (`fk_adress_id`),
-  ADD KEY `acounts_ibfk_1` (`fk_customer_id`);
+  ADD KEY `fk_adress_id` (`fk_adress_id`);
 
 --
 -- Indexes for table `companies`
 --
 ALTER TABLE `companies`
-  ADD PRIMARY KEY (`comp_id`),
-  ADD KEY `fk_adress_id` (`fk_adress_id`);
+  ADD PRIMARY KEY (`comp_id`);
 
 --
 -- Indexes for table `comp_adresses`
@@ -453,8 +516,15 @@ ALTER TABLE `comp_adresses`
 -- Indexes for table `customers`
 --
 ALTER TABLE `customers`
-  ADD PRIMARY KEY (`customer_id`),
-  ADD KEY `fk_acount_id` (`fk_acount_id`);
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Indexes for table `customer_acounts`
+--
+ALTER TABLE `customer_acounts`
+  ADD PRIMARY KEY (`fk_customer_id`,`fk_acount_id`),
+  ADD UNIQUE KEY `fk_acount_id` (`fk_acount_id`) USING BTREE,
+  ADD UNIQUE KEY `fk_customer_id` (`fk_customer_id`,`fk_acount_id`) USING BTREE;
 
 --
 -- Indexes for table `customer_adresses`
@@ -485,31 +555,41 @@ ALTER TABLE `products`
 --
 ALTER TABLE `product_company`
   ADD PRIMARY KEY (`fk_product_id`,`fk_comp_id`),
-  ADD KEY `fk_comp_id` (`fk_comp_id`);
+  ADD UNIQUE KEY `fk_product_id` (`fk_product_id`,`fk_comp_id`) USING BTREE,
+  ADD KEY `fk_comp_id` (`fk_comp_id`) USING BTREE;
 
 --
 -- Indexes for table `purchases`
 --
 ALTER TABLE `purchases`
   ADD PRIMARY KEY (`purchase_id`),
-  ADD KEY `fk_acount_id` (`fk_acount_id`),
+  ADD UNIQUE KEY `purchase_id` (`purchase_id`) USING BTREE,
   ADD KEY `fk_payment_id` (`fk_payment_id`),
-  ADD KEY `fk_product_id` (`fk_product_id`);
+  ADD KEY `purchases_ibfk_3` (`fk_product_id`);
+
+--
+-- Indexes for table `purchase_acount`
+--
+ALTER TABLE `purchase_acount`
+  ADD PRIMARY KEY (`fk_purchase_id`,`fk_acount_id`),
+  ADD UNIQUE KEY `fk_purchase_id` (`fk_purchase_id`) USING BTREE,
+  ADD KEY `fk_acount_id` (`fk_acount_id`) USING BTREE;
 
 --
 -- Indexes for table `shipments`
 --
 ALTER TABLE `shipments`
-  ADD PRIMARY KEY (`shipping_id`),
+  ADD UNIQUE KEY `fk_purchase_id` (`fk_purchase_id`) USING BTREE,
+  ADD UNIQUE KEY `shipping_id` (`shipping_id`) USING BTREE,
   ADD KEY `fk_comp_id` (`fk_comp_id`),
-  ADD KEY `fk_deliver_comp_id` (`fk_deliver_comp_id`),
-  ADD KEY `fk_purchase_id` (`fk_purchase_id`);
+  ADD KEY `fk_deliver_comp_id` (`fk_deliver_comp_id`);
 
 --
 -- Indexes for table `website`
 --
 ALTER TABLE `website`
-  ADD PRIMARY KEY (`fk_product_id`);
+  ADD PRIMARY KEY (`website_id`),
+  ADD KEY `fk_product_id` (`fk_product_id`) USING BTREE;
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -570,6 +650,12 @@ ALTER TABLE `shipments`
   MODIFY `shipping_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
+-- AUTO_INCREMENT for table `website`
+--
+ALTER TABLE `website`
+  MODIFY `website_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
 -- Constraints for dumped tables
 --
 
@@ -577,14 +663,7 @@ ALTER TABLE `shipments`
 -- Constraints for table `acounts`
 --
 ALTER TABLE `acounts`
-  ADD CONSTRAINT `acounts_ibfk_1` FOREIGN KEY (`fk_customer_id`) REFERENCES `customers` (`customer_id`) ON DELETE CASCADE ON UPDATE SET NULL,
   ADD CONSTRAINT `acounts_ibfk_2` FOREIGN KEY (`fk_adress_id`) REFERENCES `customer_adresses` (`adress_id`);
-
---
--- Constraints for table `companies`
---
-ALTER TABLE `companies`
-  ADD CONSTRAINT `companies_ibfk_1` FOREIGN KEY (`fk_adress_id`) REFERENCES `comp_adresses` (`adress_id`);
 
 --
 -- Constraints for table `comp_adresses`
@@ -593,10 +672,11 @@ ALTER TABLE `comp_adresses`
   ADD CONSTRAINT `comp_adresses_ibfk_1` FOREIGN KEY (`fk_comp_id`) REFERENCES `companies` (`comp_id`);
 
 --
--- Constraints for table `customers`
+-- Constraints for table `customer_acounts`
 --
-ALTER TABLE `customers`
-  ADD CONSTRAINT `customers_ibfk_1` FOREIGN KEY (`fk_acount_id`) REFERENCES `acounts` (`acount_id`);
+ALTER TABLE `customer_acounts`
+  ADD CONSTRAINT `customer_acounts_ibfk_1` FOREIGN KEY (`fk_customer_id`) REFERENCES `customers` (`customer_id`),
+  ADD CONSTRAINT `customer_acounts_ibfk_2` FOREIGN KEY (`fk_acount_id`) REFERENCES `acounts` (`acount_id`);
 
 --
 -- Constraints for table `product_company`
@@ -609,9 +689,16 @@ ALTER TABLE `product_company`
 -- Constraints for table `purchases`
 --
 ALTER TABLE `purchases`
-  ADD CONSTRAINT `purchases_ibfk_1` FOREIGN KEY (`fk_acount_id`) REFERENCES `acounts` (`acount_id`),
   ADD CONSTRAINT `purchases_ibfk_2` FOREIGN KEY (`fk_payment_id`) REFERENCES `payment_method` (`payment_id`),
-  ADD CONSTRAINT `purchases_ibfk_3` FOREIGN KEY (`fk_product_id`) REFERENCES `website` (`fk_product_id`);
+  ADD CONSTRAINT `purchases_ibfk_3` FOREIGN KEY (`fk_product_id`) REFERENCES `products` (`product_id`),
+  ADD CONSTRAINT `purchases_ibfk_4` FOREIGN KEY (`fk_web_id`) REFERENCES `website` (`website_id`);
+
+--
+-- Constraints for table `purchase_acount`
+--
+ALTER TABLE `purchase_acount`
+  ADD CONSTRAINT `purchase_acount_ibfk_1` FOREIGN KEY (`fk_purchase_id`) REFERENCES `purchases` (`purchase_id`),
+  ADD CONSTRAINT `purchase_acount_ibfk_2` FOREIGN KEY (`fk_acount_id`) REFERENCES `acounts` (`acount_id`);
 
 --
 -- Constraints for table `shipments`
--- BE15_CR9_Ivan_Maksimov_famazon.database+queries/be15_cr9_famzon_querries.sql
+++ BE15_CR9_Ivan_Maksimov_famazon.database+queries/be15_cr9_famzon_querries.sql
@@ -1,36 +1,37 @@
 
 
 
---  showing delivery companies from Austria - name , and ZIP code .
+-- 1 showing delivery companies from Austria - name , and ZIP code .
 
 SELECT name, adress, city, ZIP
 FROM delivering_companies
 WHERE country = "Austria";
 
 
--- showing which  products have  quantity  more than 35.
+-- 2 showing which  products have  quantity  more than 35.
 
 SELECT *
 FROM products
 WHERE products.quantity > 35;
 
 
--- showing acounts with last time login from 01.01.2019 to 01.01.2022
+-- 3 showing acounts with last time login from 01.01.2019 to 01.01.2022
 
 SELECT *
 FROM acounts
 WHERE acounts.last_login > ("2019-01-01") AND acounts.last_login < ("2022-01-01");
 
 
--- showing the purchases dates for customer with first name Marko
+-- 4 showing the purchases dates for customer with first name Marko
 
 SELECT customers.first_name, purchases.purchase_date
 FROM customers
-  JOIN purchases on purchases.fk_acount_id = customers.fk_acount_id
+  JOIN purchase_acount on purchase_acount.fk_acount_id = customers.customer_id
+  JOIN purchases on purchases.purchase_id = purchase_acount.fk_purchase_id
 WHERE first_name = "Marko";
 
 
--- showing which companies are from Amstetten.
+-- 5 showing which companies are from Amstetten.
 
 SELECT *
 FROM companies
@@ -38,16 +39,17 @@ FROM companies
 WHERE comp_adresses.city = "Amstetten";
 
 
--- showing the first name , last name, purcasing dates , product name, and product price for customer - Cristina.
+-- 6 showing the first name , last name, purcasing dates , product name, and product price for customer - Cristina.
 
 SELECT customers.first_name, last_name, purchases.purchase_date, products.name, products.price
 FROM customers
-  JOIN purchases on purchases.fk_acount_id = customers.fk_acount_id
+  JOIN purchase_acount on purchase_acount.fk_acount_id = customers.customer_id
+  JOIN purchases on purchases.purchase_id = purchase_acount.fk_purchase_id
   JOIN products on products.product_id = purchases.fk_product_id
 WHERE first_name = "Christina ";
 
 
--- showing the list of customers first name, last name, product ordered, price, shipping date and delivery date.
+-- 7 showing the list of customers first name, last name, product ordered, price, shipping date and delivery date.
 
 SELECT customers.first_name, last_name, products.name, products.price, shipments.shipp_date, shipments.delivery_date
 FROM customers
@@ -57,11 +59,12 @@ FROM customers
 WHERE purchases.purchase_date < ("2021-10-12");
 
 
--- showing the list of customers - first and last name, purchase= date and quantity, product- name and price , from wich company us the product where quantyty of purchased products is more than one.
+-- 8 showing the list of customers - first and last name, purchase= date and quantity, product- name and price , from wich company us the product where quantyty of purchased products is more than one.
 
 SELECT customers.first_name, customers.last_name, purchases.purchase_date, purchases.quantity, products.name, products.price, companies.name
 FROM customers
-  JOIN purchases on purchases.fk_acount_id = customers.fk_acount_id
+  JOIN purchase_acount on purchase_acount.fk_acount_id = customers.customer_id
+  JOIN purchases on purchases.purchase_id = purchase_acount.fk_purchase_id
   JOIN products on products.product_id = purchases.fk_product_id
   JOIN product_company ON product_company.fk_product_id = products.product_id
   JOIN companies on companies.comp_id = product_company.fk_comp_id
@@ -69,7 +72,7 @@ WHERE purchases.quantity > 1;
 
 
 
--- selecting the name and price of the product company name, adress and city delivery and purchase date 
+-- 9 selecting the name and price of the product company name, adress and city delivery and purchase date 
 
 SELECT products.name, products.price, companies.name, comp_adresses.city, shipments.delivery_date, purchases.purchase_date
 FROM products
